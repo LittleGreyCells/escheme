eb12ba39f40fe99ff2513b32122941eb46b3f77c
Larry Jordan <larry.jordan@cox.net>
2019-05-21 18:25:16 -0700

    add nomacros switch to startup

--- escheme.scm
+++ escheme.scm
@@ -1,45 +1,56 @@
 (define *version* "v1.0 (interpeter)")
-(set-prompt "ece> ")
+(set-prompt "escheme> ")
 
 (load (system-path "boot/standard-functions.scm"))
 
-(define (accume-options args options)
+(define (split-args args options files)
   (if (null? args)
-      options
+      (cons options files)
       (let ((arg (car args)))
         (if (equal? (substring arg 0 1) "-")
-            (accume-options (cdr args) (cons arg options))
-            (accume-options (cdr args) options)))))
+            (split-args (cdr args) (cons arg options) files)
+            (split-args (cdr args) options (cons arg files))
+            ))))
 
 (let ((args (cdr (vector->list (getargs))))
       (usage
        (lambda ()
-	 (display "usage: escheme [(-h | --help)] | [files...]")
+	 (display "usage: escheme [--help | --nomacros] | [files...]")
 	 (newline)))
       (boot-macros 
        (lambda ()
 	 (load (system-path "macros/macros.scm"))
 	 (load (system-path "macros/qquote.scm"))
 	 (load (system-path "boot/macro-definitions.scm")))))
-  (if (or (memv "--help" args) (memv "-h" args))
-      (begin
-        (usage)
-        (exit)))
-  (let ((options (accume-options args nil)))
-    (if options
-        (begin
-          (display "extra options not expected ")
-          (display options)
-          (newline)
-          (usage)
-          (exit))))
-  (boot-macros)
-  (let ((try-load
-         (lambda (file)
-           (let ((result (load file)))
-             (if (null? result)
-                 (error "load failed for" file))))))
-    (for-each try-load args))
-  )
+  (let ((args (split-args args nil nil))
+        (macro-rep #t)
+        options)
+    (set! options (car args))
+    (while options
+       (let ((option (car options)))
+         (cond ((or (equal? "--help" option)
+                    (equal? "-h" option))
+                (begin
+                  (usage)
+                  (exit)))
+               ((or (equal? "--nomacros" option)
+                    (equal? "-n" option))
+                (set! macro-rep #f))
+               (else
+                (display "unknown option: ")
+                (display option)
+                (newline)
+                (exit))))
+       (set! options (cdr options)))
+    (if macro-rep
+        (boot-macros)
+        (set-prompt "nomac> "))
+    (let ((try-load
+           (lambda (file)
+             (let ((result (load file)))
+               (if (null? result)
+                   (error "load failed for" file))))))
+      (for-each try-load (cdr args)))
+    ))
 
 ;; [EOF]
