c57ef01d20aa9f7e7beaacf49b5ca415e54a5d30
Larry Jordan <larry.jordan@cox.net>
2021-05-22 05:10:21 -0700

    adopt consistent naming for macro files

--- escheme.scm
+++ escheme.scm
@@ -38,7 +38,7 @@
     (set-prompt "ece> ")
     (load (system-path "boot/standard-functions.scm"))
     (load (system-path "macros/qquote.scm"))
-    (load (system-path "macros/macros.scm"))
+    (load (system-path "macros/macros-interpreter.scm"))
     (load (system-path "boot/macro-definitions.scm"))
     (load (system-path "boot/catch-throw.scm"))
     (let ((try-load
